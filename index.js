41a7836258dd6149497deac656838ea327c7c17c
danielpdaniel <artofjquinn@gmail.com>
2022-09-05 12:50:23 -0400

    commit tree 4bac8cc4a3fa01a1f0d86ac1580e919d33056263

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    // console.log(`sums of ${i}`)
+    for(let n = i + 1; n < array.length; n++){
+      // console.log(array[i] + array[n])
+      if (array[i] + array[n] === target){
+        return true;
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  so it would be... (n steps (depending on array) * n steps)  ? 
+  since its cycling through the array and basically cycling through 
+  the array for each array element it cycles through? so either...
+   O(n) or O(n^2) ?
 */
 
 /* 
   Add your pseudocode here
+  
+  function (array, target)
+  let n = 1
+    for each integer of array
+      if array[i] + array[n] === target
+      return true
+      n + 1
 */
 
 /*
   Add written explanation of your solution here
+  -need to write a function that takes an array of numbers and returns
+  true if the sum of any two of those numbers is equal to the target
+  value that also gets entered into the function after the array
+  -if we were to go straight ahead and compare every possible sum in
+  order until/if we found a match, then each number in the array would
+  only have to find the sums after it since the ones before would
+  already have been tested. so maybe a for loop where array[i] is the integer
+  we're on and then theres a nested for loop in there that then adds
+  each array integer after the current one? then it steps back out 
+  to the outer for loop, moves to the next integer in the array and 
+  adds all the numbers after that one?
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +62,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: false")
+  console.log("=>", hasTargetSum([6], 6));
+
+  console.log("Expecting: true")
+  console.log("=>", hasTargetSum([6, 0], 6))
 }
 
 module.exports = hasTargetSum;
